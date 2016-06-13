3a8b0fb7ae03d679459627d04433a196b59966df
Rengaraj <sakthirengaraj@gmail.com>
2016-06-13 14:48:26 +0530

    hard coded event message or event name is removed and event name option is added

--- event-countdown/countdown.py
+++ event-countdown/countdown.py
@@ -1,6 +1,8 @@
-"""Wedding Count Down
+"""Event Count Down
+This program will give the no of days left for any event
+if the event date is given.
 
-Usage: countdown.py <date>
+Usage: countdown.py <date> <event-name>
 
 Options:
  -h --help
@@ -15,16 +17,17 @@ import notify2
 
 args = docopt(__doc__)
 Date =  re.split(' |-|/', args['<date>'])
+event_name = args['<event-name>']
+
 D = int(Date[0])
 M = int(Date[1])
 Y = int(Date[2])
 today = date.today()
-my_weddingday = date(today.year, M, D)
-count = abs(my_weddingday - today)
-print count.days
-#Message(text=str(count.days) + "days to go")
+event_day = date(today.year, M, D)
+count = abs(event_day - today)
+
 notify2.init("Basic")
-n = notify2.Notification("Rengaraj weds Jayasri",
+n = notify2.Notification(event_name,
                          str(count.days) + 'Days to Go' + "(:-)",
                          "notification-message-im"   # Icon name
                          )
