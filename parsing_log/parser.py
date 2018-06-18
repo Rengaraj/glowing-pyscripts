79e7f1aa158dd9ac854666b768a7b40f72fe3544
gnurenga <gnurenga@outlook.com>
2018-06-18 14:24:44 +0530

    doctsting for each function is added

--- parsing_log/parser.py
+++ parsing_log/parser.py
@@ -1,15 +1,29 @@
 """
-ip:numbytes
+Parser.py  parse http.log and sum the numbytes each unique IP.
 """
-from collections import defaultdict
 
 def  add_numbytes(data, ip, numbytes):
+     """ add numbytes will find unique ip and sum the numbytes. if
+     the ip is not present it will creat the ip as key and assign numbytes
+     
+     args:
+         data - dictionary
+         ip   -  ip as a key (string)
+         numbytes - string
+
+     returns:
+            None
+     """
      if data.has_key(ip):
          data[ip] = data[ip] + int(numbytes)
      else:
          data[ip] = int(numbytes)
 
 def main():
+     """ open the log and read the file.
+     Discard the first line and split rest of the each line.
+     Then use only ip and numbytes from each line.
+     """
      with open('http.log', 'r') as f:
           data = f.readlines()     
 
@@ -24,5 +38,6 @@ def main():
 
      print unique_ip
 
-main()
+if __name__ == "__main__":
+     main()
 
